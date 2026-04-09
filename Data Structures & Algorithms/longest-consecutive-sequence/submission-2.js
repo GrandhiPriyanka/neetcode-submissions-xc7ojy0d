ca9f5161e9b5aaff6bed2f9ce928c567250687dc
Priyanka Grandhi <grandhi.ragapriyanka@gmail.com>
2026-04-09 15:58:52 +0530

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const set = new Set(nums);
+        let maxLength = 0;
+        for (const num of set){
+            // if(!set.has(num-1)){
+                let current = num;
+                let currentLength = 1;
+                while(set.has(current + 1) ){
+                    current++;
+                    currentLength++
+                }
+                maxLength = Math.max(maxLength, currentLength);
+            // }
+
+        }
+        return maxLength;
+    }
+}
