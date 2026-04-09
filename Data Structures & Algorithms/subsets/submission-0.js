f41ed742b52d0c9580e5468d09d04bf47b76ced8
Priyanka Grandhi <grandhi.ragapriyanka@gmail.com>
2026-04-09 07:11:04 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums) {
+        let result=[];
+        function backTrack(start,current){
+            result.push([...current]);
+            for(let i = start;i<nums.length;i++){
+                current.push(nums[i]);
+                backTrack(i+1,current);
+                current.pop();
+            }
+        }
+        backTrack(0,[]);
+        return result;
+    }
+}
